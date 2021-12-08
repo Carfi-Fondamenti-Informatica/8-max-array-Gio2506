708bf86493b691e5d3a18d5ff0212ad5048f512e
Gio2506 <91522962+Gio2506@users.noreply.github.com>
2021-12-08 12:13:12 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
-  return 0;
+    int a=0;
+    float num=0;
+    cin>> a;
+    if(a<=0){
+        return 0;
+    } float numeri[a];
+    for (int i = 0; i < a; i++) {
+        num = 0;
+        cin >> num;
+        numeri[i] = num;
+    }
+    cout<<numMagg(numeri,a)<<endl;
+    return 0;
 }
+
+
