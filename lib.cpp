c0faa72edcd684115305484a0252b7fc8db0e00e
Gio2506 <91522962+Gio2506@users.noreply.github.com>
2021-12-08 12:12:30 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,13 @@
+#include "lib.h"
 
+float numMagg(float numeri[],int dim){
+    float b=0;
+    b=numeri[0];
+    for(int j=0;j<dim;j++){
+        if(b<numeri[j]){
+            b=numeri[j];
+        }
+    }
+    return b;
+
+}
